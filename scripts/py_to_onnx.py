33707df0c2d59a5122b8df5231d5d32a532e040d
Diego Cividini <cividini.diego.dev@gmail.com>
2026-06-26 10:55:27 +0200

    fix conversion script to work on Python 3.14

--- scripts/py_to_onnx.py
+++ scripts/py_to_onnx.py
@@ -9,17 +9,17 @@ pytorch_net.eval()
 pytorch_net = pytorch_net.float()
 
 # cpsam wants a fixed input size of 256x256
-dummy_input = torch.randn(1, 3, 256, 256, dtype=torch.float32) 
+dummy_input = torch.randn(1, 3, 256, 256, dtype=torch.float32)
 
 # ONNX export
 torch.onnx.export(
-    pytorch_net, 
-    dummy_input, 
-    f"{model_name}.onnx", 
+    pytorch_net,
+    dummy_input,
+    f"{model_name}.onnx",
     export_params=True,
-    opset_version=17,
-    do_constant_folding=True,
-    input_names=['input_image'], 
+    opset_version=18,
+    do_constant_folding=False,
+    input_names=['input_image'],
     output_names=['flows_and_probs']
 )
 
